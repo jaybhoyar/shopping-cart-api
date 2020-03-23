89d5c79b4ec9cc983140e2a597af92f98c2d9f06
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-23 23:11:45 +0530

    register

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const userController 
+const userController = require("../../controllers/user");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
 	res.json({ Response: "Welcome to Users Route" });
 });
 router.post("/register", userController.register);
+router.post("/login", userController.login);
 
 module.exports = router;
