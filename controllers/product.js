39b8c6261420d72a9d953cadffd1ad8f9a7d7b54
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-25 22:39:15 +0530

    new product

--- controllers/product.js
+++ controllers/product.js
@@ -5,9 +5,9 @@ const auth = require("../modules/auth");
 
 exports.newProduct = async (req, res, next) => {
 	try {
-		res.json({ msg: "this is new product" });
+		var product = await Product.create(req.body.product);
+		res.status(200).json({ newProduct: product });
 	} catch (error) {
 		next(error);
 	}
 };
-
--- routes/v1/products.js
+++ routes/v1/products.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../../controllers/product");
+const auth = require("../../modules/auth");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
 	res.json({ Response: "Welcome to Products Route" });
 });
-router.get("/new", productController.newProduct);
+router.post("/new", auth.validateJWT, productController.newProduct);
 module.exports = router;
