5d430234b526466e6844438436f7fc68cadff032
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-25 23:07:06 +0530

    all products

--- controllers/product.js
+++ controllers/product.js
@@ -5,8 +5,17 @@ const auth = require("../modules/auth");
 
 exports.newProduct = async (req, res, next) => {
 	try {
-		var product1 = await Product.create(req.body.product);
-		res.status(200).json({ newProduct: product1 });
+		var product = await Product.create(req.body.product);
+		res.status(200).json({ newProduct: product });
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.allProduct = async (req, res, next) => {
+	try {
+		var allProducts = await Product.find({});
+		res.status(200).json({ Products: allProducts });
 	} catch (error) {
 		next(error);
 	}
--- routes/v1/products.js
+++ routes/v1/products.js
@@ -8,4 +8,6 @@ router.get("/", (req, res, next) => {
 	res.json({ Response: "Welcome to Products Route" });
 });
 router.post("/new", auth.validateJWT, productController.newProduct);
+router.get("/all", auth.validateJWT, productController.allProduct);
+
 module.exports = router;
