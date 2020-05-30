c8d89a0ecfacb26842947cf29654ba54e6a652b6
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-05-30 16:17:36 +0530

    fix: add product id

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ mongoose.connect(
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-		useFindAndModify: false
+		useFindAndModify: false,
+		createIndexes: true,
 	},
-	err => {
+	(err) => {
 		console.log(err ? err : "Connected......");
 	}
 );
@@ -34,19 +35,19 @@ app.use("/", indexRouter);
 app.use("/api/v1", v1Router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get("env") === "development" ? err : {};
 
 	// render the error page
 	res.status(err.status || 500);
-	res.json({ error: err });
+	res.json({ err });
 });
 
 module.exports = app;
--- controllers/cart.js
+++ controllers/cart.js
@@ -6,58 +6,50 @@ const Product = require("../models/product");
 exports.allItems = async (req, res, next) => {
 	try {
 		var user = await User.findById(req.userId);
-		var cart = await (await Cart.findById(user.cartId))
-			.populate({
-				path: "items",
-				populate: {
-					path: "productId",
-					model: "Product",
-				},
-			})
-			.execPopulate();
-
+		var cart = await await Cart.findById(user.cartId);
+    console.log(cart.items);
+    
 		res.status(200).json({ cart });
 	} catch (error) {
 		next(error);
 	}
 };
+
 exports.addItem = async (req, res, next) => {
 	try {
 		var user = await User.findById(req.userId);
 		var product = await Product.findById(req.params.id);
 		var items = await Item.find({ name: product.name });
 		var res = items.find((obj) => obj.name == product.name);
-		console.log(res);
 		if (res) {
 			var item = await Item.findOneAndUpdate(
 				{ name: product.name },
 				{ $inc: { quantity: 1 } },
 				{ new: true }
 			);
-			console.log("----<>------", item);
+			console.log("-----", item);
 		} else {
 			var item = await Item.create({
 				productId: req.params.id,
 				name: product.name,
 				quantity: 1,
 			});
-			console.log("+++++++", item);
+			console.log("-----", item);
 		}
-
-		// var cart = await Cart.findByIdAndUpdate(
-		// 	user.cartId,
-		// 	{
-		// 		$addToSet: { items: item.id },
-		// 	},
-		// 	{ new: true }
-		// );
-		// console.log(cart);
-
-		res.status(200).json({ items });
+		var cart = await Cart.findByIdAndUpdate(
+			user.cartId,
+			{
+				$addToSet: { items: item.productId },
+			},
+			{ new: true }
+		);
+		console.log("---------", cart.items);
+		res.json({ item });
 	} catch (error) {
 		next(error);
 	}
 };
+
 exports.deleteItem = async (req, res, next) => {
 	try {
 		var user = await User.findById(req.userId);
